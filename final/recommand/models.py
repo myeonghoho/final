dda77f6d4d1f14ea471797cd6c1b47dcdea03862
minsu <freekek67@naver.com>
2024-05-16 17:43:43 +0900

    5/16 웹페이지
    어드민 만들고 템플릿 조금 나눔

--- final/recommand/models.py
+++ final/recommand/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class upload_image(models.Model) :
-    upload_image = models.FileField(upload_to = 'Uploaded Files/%y/%m/%d/', blank=True)
-    컬럼_업로드날짜 = models.DateField(auto_now = True)
+class Upload_image(models.Model) :
+    title = models.CharField("제목", max_length = 100, blank = True)
+    upload_image = models.ImageField("이미지", upload_to = "media/images/", blank = True)
+
+    def __str__(self):
+        return self.title
