e4e658d42b92d20db50b450bd717276390860efe
myeonghoho <159095053+myeonghoho@users.noreply.github.com>
2024-05-17 09:29:57 +0900

    Delete final directory

--- final/recommand/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from recommand.models import Upload_image
-
-# Register your models here.
-@admin.register(Upload_image)
-class Upload_imageAdmin(admin.ModelAdmin):
-    list_display = ["title", "upload_image"]
